ae29c827f5978de8ee8f95ac095fd0e824ea1319
Kasu <icesoy99@gmail.com>
2026-03-06 22:55:02 +0700

    Refactor: change and added migration and new readme.md

---
+++ IoTe-website/database/migrations/2026_03_06_154447_create_course_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('course', function (Blueprint $table) {
+            $table->string('courseID', 20)->primary();
+            $table->integer('courseYear');
+            $table->string('courseName', 255);
+            $table->integer('courseCredit');
+            $table->string('courseType', 50);
+            $table->text('courseDescript')->nullable();
+            $table->integer('courseSemester');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('course');
+    }
+};
